7f1b4c4847c37f2b076939f600a8f6aeb4c4a170
Agustin Suarez <suarez.agu@gmail.com>
2015-05-09 15:23:13 -0400

    Stuff

--- src/HuffmanCompression.java
+++ src/HuffmanCompression.java
@@ -1,5 +1,27 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class HuffmanCompression{
     public static void main(String[] args){
-        System.out.println("yo");
+        try{
+            FileReader file = new FileReader("/Users/slus/Desktop/hello.txt");
+            char[][] charArray = new char[256][2];
+            int chars = file.read();
+            while(chars != -1){
+
+            }
+
+        }catch(IOException e){
+            e.printStackTrace();
+            System.out.println("The file you have requested does not exist");
+
+        }
+    }
+
+    public void compress(File file){
+
     }
 }
